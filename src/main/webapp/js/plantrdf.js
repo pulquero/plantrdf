5a99856973c15684ff4d5e200aa9f7900a06f165
Mark Hale <mj_hale@yahoo.com>
2016-05-07 19:18:08 +0100

    form fix

--- src/main/webapp/js/plantrdf.js
+++ src/main/webapp/js/plantrdf.js
@@ -1,23 +1,3 @@
-d3sparql.update = function(endpoint, sparql, callback) {
-  var url = endpoint
-  if (d3sparql.debug) { console.log(endpoint) }
-  if (d3sparql.debug) { console.log(url) }
-  d3.xhr(url)
-  .header("Content-Type", "application/x-www-form-urlencoded")
-  .post("update=" + encodeURIComponent(sparql), callback)
-};
-
-d3sparql.ask = function(endpoint, sparql, callback) {
-  var url = endpoint + "?query=" + encodeURIComponent(sparql)
-  if (d3sparql.debug) { console.log(endpoint) }
-  if (d3sparql.debug) { console.log(url) }
-  var mime = "text/boolean"
-  d3.xhr(url, mime, function(request) {
-    var txt = request.responseText
-    if (d3sparql.debug) { console.log(txt) }
-    callback(txt == 'true')
-  })
-};
 
 function getQueryEndpoint(garden) {
 	return SESAME_URL + "repositories/" + garden;
